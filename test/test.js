dae8edf1f09542c8492ba04597a57f295e99a08c
akhil0989 <techakhil.09@gmail.com>
2025-04-17 16:50:47 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,9 @@
+const request = require('supertest');
 const app = require('../app');
 
-test('Hello endpoint returns correct message', () => {
-  const req = {};
-  const res = {
-    json: jest.fn()
-  };
+test('Hello endpoint returns correct message', async () => {
+  const res = await request(app).get('/');
   
-  app.get('/', (req, res));
-  
-  expect(res.json).toHaveBeenCalledWith({ message: 'Hello from Custom Build Environment!' });
+  expect(res.statusCode).toBe(200);
+  expect(res.body).toEqual({ message: 'Hello from Custom Build Environment!' });
 });
